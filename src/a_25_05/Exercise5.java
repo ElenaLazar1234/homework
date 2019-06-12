938235dc588acc05a125140d6ea04e7b913f38f2
ionutspalatelu@gmail.com <ionutspalatelu@gmail.com>
2019-06-12 16:04:02 +0300

    Verificare tema 1

--- src/a_25_05/Exercise5.java
+++ src/a_25_05/Exercise5.java
@@ -1,6 +1,10 @@
 package a_25_05;
+
+import java.util.Scanner;
+
 public class Exercise5 {
 
+    /*
     public static void main(String[] args) {
         int a = 50;
         int d = 2;
@@ -21,6 +25,27 @@ public class Exercise5 {
             }
         }
     }
+    */
+
+    //Ce zici de varianta asta?
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        for (int i = n; i >= 2; i--) {
+            if (isPrime(i)) //verifici daca e prim
+                System.out.println(i);
+        }
+    }
+
+    private static boolean isPrime(int i) {
+        for (int j = 2; j < i / 2; j++) {
+            if (i % j == 0)
+                return false;
+        }
+        return true;
+    }
+    //PS: a ta nu functiona corect
 }
 
 
